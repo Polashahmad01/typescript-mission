e4ffcb71fa225836cae23fecc9c531a9d50c6d0d
Polashahmad01 <dev.polashahmad@gmail.com>
2025-06-25 13:29:18 +0600

    learn about basic data types

--- mission-01/src/01-first-program.ts
+++ mission-01/src/01-first-program.ts
@@ -1,3 +1,9 @@
-let missionName: string = 'I am starting the TypeScript learning mission!';
+{
+  //
 
-console.log({ missionName });
+  let missionName: string = 'I am starting the TypeScript learning mission!';
+  
+  console.log({ missionName });
+  
+  //
+}
---
+++ mission-01/src/02-basic-data-types.ts
@@ -0,0 +1,30 @@
+{
+  //
+
+  // String
+  let myFirstName: string = 'Istiak';
+
+  // Number
+  let myNidNumber: number = 24535;
+
+  // Boolean
+  let isMarried: boolean = false;
+
+  // Undefined
+  let permanentAddress: undefined = undefined;
+
+  // Null
+  let person: null = null;
+
+  // Array
+  const stringArray: string[] = ['john', 'kate', 'nancy'];
+
+  const rollNumberArray: number[] = [23425, 235, 3462];
+
+  // Tuple
+  const coordinatesTuple: [number, number] = [20, 30];
+
+  const nameAgeTuple: [string, number] = ['john', 30];
+
+  //
+}
