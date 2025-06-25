f02556f103e06750482182a42214530e817a70bf
Polashahmad01 <dev.polashahmad@gmail.com>
2025-06-25 13:44:24 +0600

    learn about object in typescript

---
+++ mission-01/src/03-object-optional-literal.ts
@@ -0,0 +1,19 @@
+{
+  //
+
+  const user: {
+    readonly company: 'SoftEra'; // Literal types
+    firstName: string;
+    middleName?: string; // Optional types
+    lastName: string;
+    isMarried: boolean;
+  } = {
+    company: 'SoftEra',
+    firstName: 'Istiak',
+    middleName: 'Ahmad',
+    lastName: 'Polash',
+    isMarried: true
+  }
+
+  //
+}
